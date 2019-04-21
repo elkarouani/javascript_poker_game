60e9dfc8fecfb7515e16f07d352056e6402903e6
KDragon <elkarouani@gmail.com>
2019-04-21 21:48:22 +0100

    Task: ask for played amount functionality in game beginning

---
+++ app.js
@@ -0,0 +1,10 @@
+// Caching Dom
+const playerAmount = document.getElementById("playerAmount").firstChild;
+
+// Methods
+
+// Main
+// console.log(playerAmount.data);
+playerAmount.data = prompt("\t\t\t     New game : \n\n Enter an amount number of money to start : \n\n");
+
+// Events
