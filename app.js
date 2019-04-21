24072796fed85c9fdfac6f40d3299b2e8b2d110b
KDragon <elkarouani@gmail.com>
2019-04-21 22:26:37 +0100

    Task: adding give random cards to player functionality

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Caching Dom
 const playerAmount = document.getElementById("playerAmount").firstChild;
+const playerFirstCardNumber = document.getElementById("playerFirstCardNumber").firstChild;
+const playerFirstCardSymbol = document.getElementById("playerFirstCardSymbol").firstChild;
+const playerSecondCardNumber = document.getElementById("playerSecondCardNumber").firstChild;
+const playerSecondCardSymbol = document.getElementById("playerSecondCardSymbolr").firstChild;
 const cardsMap = [
 				[ "♣", ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] ],
 				[ "♥", ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] ],
@@ -7,17 +11,24 @@ const cardsMap = [
 				[ "♠", ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] ]
 				];
 
-
 // Methods
 const getRandomCard = () => {
-	let symbolCollection = cardsMap[Math.floor((Math.random() * cardsMap.length) + 1)];
-	let number = symbolCollection[1][Math.floor((Math.random() * symbolCollection.length) + 1)]
-	console.log(symbolCollection[0]);
-	console.log(number);
+	let symbolCollection = cardsMap[Math.floor((Math.random() * cardsMap.length))];
+	let number = symbolCollection[1][Math.floor((Math.random() * symbolCollection[1].length))]
+	return new Array(number, symbolCollection[0]);
+}
+
+const game = () => {
+	playerAmount.data = prompt("\t\t\t     New game : \n\n Enter an amount number of money to start : \n\n");
+	let firstPlayerCard = getRandomCard();
+	let secondPlayerCard = getRandomCard();
+	playerFirstCardNumber.data = firstPlayerCard[0];
+	playerFirstCardSymbol.data = firstPlayerCard[1];
+	playerSecondCardNumber.data = secondPlayerCard[0];
+	playerSecondCardSymbol.data = secondPlayerCard[1];
 }
 
 // Main
-// console.log(playerAmount.data);
-playerAmount.data = prompt("\t\t\t     New game : \n\n Enter an amount number of money to start : \n\n");
-getRandomCard();
+game();
+
 // Events
